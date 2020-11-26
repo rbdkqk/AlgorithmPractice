5754e837164c05341c3990f61d6804fc0e0b52b1
rbdkqk <rbdkqk@gmail.com>
2020-11-27 00:49:33 +0900

    각 파일명 앞에 난이도 표시함

--- Programers/해시/(1) 완주하지 못한 선수.js
+++ Programers/해시/(1) 완주하지 못한 선수.js
@@ -1,60 +1,77 @@
-/* 
-  <문제 설명>
+/* 완주하지 못한 선수 (푼 날짜 기억 안남)
+  
+  '해시' 파트, 난이도 1단계  -  (푼 시간 기억 안남)
+
+
+  1. 문제 설명
+    
     수많은 마라톤 선수들이 마라톤에 참여하였습니다. 
     단 한 명의 선수를 제외하고는 모든 선수가 마라톤을 완주하였습니다.
 
     마라톤에 참여한 선수들의 이름이 담긴 배열 participant와 완주한 선수들의 이름이 담긴 배열 completion이 주어질 때, 
     완주하지 못한 선수의 이름을 return 하도록 solution 함수를 작성해주세요.
 
-  <제한사항>
+
+  2. 제한사항
+
     마라톤 경기에 참여한 선수의 수는 1명 이상 100,000명 이하입니다.
     completion의 길이는 participant의 길이보다 1 작습니다.
     참가자의 이름은 1개 이상 20개 이하의 알파벳 소문자로 이루어져 있습니다.
     참가자 중에는 동명이인이 있을 수 있습니다. 
+
+
+  3. 입출력 예
+    
+    participant	: [leo, kiki, eden]
+    completion : [eden, kiki]
+    return : "leo"
+    
+    participant	: [marina, josipa, nikola, vinko, filipa]
+    completion : [josipa, filipa, marina, nikola]
+    return : "vinko"
+    
+    participant	: [mislav, stanko, mislav, ana]
+    completion : [stanko, ana, mislav]
+    return : "mislav"
 */
-
-
 
 /* 시도 (1) : 작은 테스트케이스는 통과하지만, 채점에서 실패함.
 
-function solution(participant, completion) {
-  var answer = '';    
-  
-  for (let i = 0; i < completion.length; i++) {
-      for (let j = 0; j < participant.length; j++) {
-          if (completion[i] === participant[j]) {
-              participant.splice(j, 1);
-          }
-      }
-  }
-  
-  answer = participant[0];
-  
-  return answer;
-
-} 
+  function solution(participant, completion) {
+    var answer = '';    
+    
+    for (let i = 0; i < completion.length; i++) {
+        for (let j = 0; j < participant.length; j++) {
+            if (completion[i] === participant[j]) {
+                participant.splice(j, 1);
+            }
+        }
+    }
+    
+    answer = participant[0];
+    
+    return answer;
+  } 
 
 */
-
 
 /* 시도 (2) : 통과됨 */
 
 function solution(participant, completion) {
-  var answer = '';    
-    
+  var answer = "";
+
   participant.sort();
   completion.sort();
 
   for (let i = 0; i < participant.length; i++) {
-    if(participant[i] !== completion[i]) {
-        answer = participant[i];
-        break;
+    if (participant[i] !== completion[i]) {
+      answer = participant[i];
+      break;
     }
-  }    
-    
+  }
+
   return answer;
 }
-
 
 /* 추가 공부할만한 내용들 정리
 
